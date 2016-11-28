2997418b67470f925221d88895b97c54191b4f21
Paul FIGIEL <paulfigiel@laposte.net>
2016-11-28 21:54:32 +0100

    + de mission et + d'amour

--- Assets/Scripts/SenpaiController.cs
+++ Assets/Scripts/SenpaiController.cs
@@ -9,12 +9,39 @@ public class SenpaiController : MonoBehaviour {
     private int _currentdirection;
     private int _health;
     private string _whatimsaying = "";
+    // Paramètres missions
+    public float missionTime = 10;
+    public float missionTextTime = 5;
+    public int PF_lost = 100;
     // to check if a mission is currently launched
     private bool _MissionDestroyWeakpoint = false;
+    public string destroyWeakpointPhrase = "";
+    // mission attack sun
     private bool _MissionAttackSun = false;
+    private int[] _playersTouchedSun = new int[2];
+    public string attackSunPhrase = "";
+    public int SunAttackAmount = 10;
+    // mission stay near me
+    private bool _MissionStayNearMe =false;
+    public string staynearmePhrase ="";
+    public float stayCloseToMeTime = 5;
+    private float _P1Entered = Mathf.Infinity; // time when P1 entered the zone around SENPAIIIIIIIIII
+    private float _P2Entered = Mathf.Infinity;
+    // Mission Stop Fireing
+    private bool _MissionStopFireing = false;
+    public string stopFireingPhrase = "";
+    private bool _P1Fired;
+    private bool _P2Fired;
+    // Mission getAway
+    private bool _MissionGetAway = false;
+    public string getAwayPhrase = "";
+    public float getAwayTime = 5;
+    private float _P1EnteredOpposed = Mathf.Infinity; // time when P1 entered the zone around SENPAIIIIIIIIII
+    private float _P2EnteredOpposed = Mathf.Infinity;
     // Use this for initialization
     void Start () {
 	    _health = startingHealth;
+        startMissionDestroyWeakpoint(10);
 	}
 	
 	// Update is called once per frame
@@ -32,8 +59,16 @@ public class SenpaiController : MonoBehaviour {
         {
             //don't move
         }
-        startRandomMission();
-	}
+
+        Vector3 diff = new Vector3(0,0,0) - transform.position;
+        diff.Normalize();
+
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z +90);
+        GameObject.Find("OpposedZoneAnchor").transform.rotation = Quaternion.Euler(0f, 0f, rot_z );
+
+        //startRandomMission();
+    }
     void moveRandom()
     {
         if (Random.Range(0,100)==0)
@@ -63,17 +98,9 @@ public class SenpaiController : MonoBehaviour {
         GUI.skin = messages;
         if (_whatimsaying!="")
         {
-            Vector3 v = Camera.main.WorldToScreenPoint(new Vector3(transform.position.x, -transform.position.y, transform.position.z));
-            // Avoid clipping
-            if (v.y < 10)
-            {
-                v.y = 10;
-            }
-            else if (v.y + 20 > Screen.height)
-            {
-                v.y = Screen.height - 20;
-            }
-            GUI.Box(new Rect(v.x, v.y, 100, 80), _whatimsaying);
+            Vector3 v = Camera.main.WorldToScreenPoint(new Vector3(0,0,0));
+            Vector2 v2 = Vector2.Lerp(v,transform.position,0.5f);
+            GUI.Box(new Rect(v2.x, v2.y, 100, 80), _whatimsaying);
         }
     }
     void say(string whatisay,float timelasting)
@@ -82,19 +109,16 @@ public class SenpaiController : MonoBehaviour {
     }
     IEnumerator initSaying(float time,string text)
     {
-        for (int i =0;i<text.Length;i++)
-        {
-            _whatimsaying +=text[i];
-            yield return new WaitForSeconds(0.1f);
-        }
+        _whatimsaying =text;
         yield return new WaitForSeconds(time);
         _whatimsaying = "";
     }
     // Missions d'entraide ///////////////////////////////////
+    // Misssion Destroy Weakpoint ///////////////////////////////////
     void startMissionDestroyWeakpoint(float tempsmission)
     {
         _MissionDestroyWeakpoint = true;
-        say("Détruisez le point faible !",5);
+        say(destroyWeakpointPhrase, missionTextTime);
         StartCoroutine(checkMissionDestroyWeakpoint(tempsmission));
     }
     IEnumerator checkMissionDestroyWeakpoint(float temps)
@@ -107,6 +131,7 @@ public class SenpaiController : MonoBehaviour {
         if (_MissionDestroyWeakpoint)
         {
             GameObject.Find("Player1").GetComponent<PlayerBehavior>()._PF += 100;
+            GameObject.Find("Player2").GetComponent<PlayerBehavior>()._PF += 100;
             _MissionDestroyWeakpoint = false;
         }
     }
@@ -114,23 +139,171 @@ public class SenpaiController : MonoBehaviour {
     {
         if (_MissionDestroyWeakpoint)
         {
+            GameObject.Find("Player1").GetComponent<PlayerBehavior>()._PF += 100;
             GameObject.Find("Player2").GetComponent<PlayerBehavior>()._PF += 100;
             _MissionDestroyWeakpoint = false;
         }
     }
+    // Misssion Attack sun ///////////////////////////////////
     void startMissionAttackSun(float tempsmission)
     {
         _MissionAttackSun = true;
-        say("Attaquez le !", 5);
-        StartCoroutine(checkMissionDestroyWeakpoint(tempsmission));
+        say(attackSunPhrase, missionTextTime);
+        StartCoroutine(checkMissionAttackSun(tempsmission));
     }
     IEnumerator checkMissionAttackSun(float temps)
     {
         yield return new WaitForSeconds(temps);
+        if (_playersTouchedSun[0] > SunAttackAmount)
+        {
+            GameObject.Find("Player1").GetComponent<PlayerBehavior>()._PF += 100;
+        }
+        else
+        {
+            GameObject.Find("Player1").GetComponent<PlayerBehavior>()._PF -= PF_lost;
+        }
+        if (_playersTouchedSun[1] > SunAttackAmount)
+        {
+            GameObject.Find("Player2").GetComponent<PlayerBehavior>()._PF += 100;
+        }
+        else
+        {
+            GameObject.Find("Player2").GetComponent<PlayerBehavior>()._PF -= PF_lost;
+        }
+        _playersTouchedSun[0] = 0;
+        _playersTouchedSun[1] = 0;
         _MissionAttackSun = false;
     }
+    void OnP1AttackSun()
+    {
+        _playersTouchedSun[0]++;
+    }
+    void OnP2AttackSun()
+    {
+        _playersTouchedSun[1]++;
+    }
     // Missions de protection ///////////////////////////////////
+    // Misssion stay near me ///////////////////////////////////
+    void startMissionStayNearMe(float tempsmission)
+    {
+        _MissionStayNearMe = true;
+        _P1Entered = Mathf.Infinity;
+        _P2Entered = Mathf.Infinity;
+        say(staynearmePhrase, missionTextTime);
+        StartCoroutine(checkMissionStayNearMe(tempsmission));
+    }
+    IEnumerator checkMissionStayNearMe(float temps)
+    {
+        yield return new WaitForSeconds(temps);
+        if (Time.time - _P1Entered > stayCloseToMeTime)
+        {
+            GameObject.Find("Player1").GetComponent<PlayerBehavior>()._PF += 100;
+        }
+        else
+        {
+            GameObject.Find("Player1").GetComponent<PlayerBehavior>()._PF -= PF_lost;
+        }
+        if (Time.time - _P2Entered > stayCloseToMeTime)
+        {
+            GameObject.Find("Player2").GetComponent<PlayerBehavior>()._PF += 100;
+        }
+        else
+        {
+            GameObject.Find("Player2").GetComponent<PlayerBehavior>()._PF -= PF_lost;
+        }
+        _MissionStayNearMe = false;
+
+    }
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.name == "Player1")
+        {
+            _P1Entered = Time.time;
+        }
+        else if (coll.gameObject.name == "Player2")
+        {
+            _P2Entered = Time.time;
+        }
+    }
     // Missions caprice ///////////////////////////////////
+    // Misssion stay near me ///////////////////////////////////
+    void startMissionStopFireing(float tempsmission)
+    {
+        _MissionStopFireing = true;
+        say(stopFireingPhrase, missionTextTime);
+        StartCoroutine(checkMissionStopFireing(tempsmission));
+    }
+    IEnumerator checkMissionStopFireing(float temps)
+    {
+        yield return new WaitForSeconds(1);
+        _P1Fired = false;
+        _P2Fired = false;
+        yield return new WaitForSeconds(temps);
+        _MissionStopFireing = false;
+        if (_P1Fired)
+        {
+            GameObject.Find("Player1").GetComponent<PlayerBehavior>()._PF -= PF_lost;
+        }
+        else
+        {
+            GameObject.Find("Player1").GetComponent<PlayerBehavior>()._PF += 100;
+        }
+        if (_P2Fired)
+        {
+            GameObject.Find("Player2").GetComponent<PlayerBehavior>()._PF -= PF_lost;
+        }
+        else
+        {
+            GameObject.Find("Player2").GetComponent<PlayerBehavior>()._PF += 100;
+        }
+    }
+    void OnP1shoot()
+    {
+        _P1Fired = true;
+    }
+    void OnP2shoot()
+    {
+        _P2Fired = true;
+    }
+    // Mission Get Away //////////////////////////////////////////////////
+    void startMissionGetAway(float tempsmission)
+    {
+        _MissionGetAway = true;
+        _P1EnteredOpposed = Mathf.Infinity;
+        _P2EnteredOpposed = Mathf.Infinity;
+        say(getAwayPhrase, missionTextTime);
+        StartCoroutine(checkMissionGetAway(tempsmission));
+    }
+    IEnumerator checkMissionGetAway(float temps)
+    {
+        yield return new WaitForSeconds(temps);
+        if (Time.time - _P1EnteredOpposed > getAwayTime)
+        {
+            GameObject.Find("Player1").GetComponent<PlayerBehavior>()._PF += 100;
+        }
+        else
+        {
+            GameObject.Find("Player1").GetComponent<PlayerBehavior>()._PF -= PF_lost;
+        }
+        if (Time.time - _P2EnteredOpposed > getAwayTime)
+        {
+            GameObject.Find("Player2").GetComponent<PlayerBehavior>()._PF += 100;
+        }
+        else
+        {
+            GameObject.Find("Player2").GetComponent<PlayerBehavior>()._PF -= PF_lost;
+        }
+        _MissionGetAway = false;
+    }
+    void OnP1EnterOpposed()
+    {
+        _P1EnteredOpposed = Time.time;
+    }
+    void OnP2EnterOpposed()
+    {
+        _P2EnteredOpposed = Time.time;
+    }
+    //////////////////////////////////////////////////////////////////////
     void startRandomMission()
     {
         if (!_MissionDestroyWeakpoint && !_MissionAttackSun)
@@ -138,11 +311,11 @@ public class SenpaiController : MonoBehaviour {
             int mission = Random.Range(1,3);
             if (mission == 1)
             {
-                startMissionDestroyWeakpoint(10);
+                startMissionDestroyWeakpoint(missionTime);
             }
             else if (mission == 2)
             {
-                startMissionAttackSun(10);
+                startMissionAttackSun(missionTime);
             }
         }
     }
