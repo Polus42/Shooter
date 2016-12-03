0278a45ff11c54ec06c4cfc8b69c52208fdb861d
Kkkkkkkevin <kev.l.50@hotmail.fr>
2016-12-03 15:49:47 +0100

    score en cours

---
+++ Assets/Scripts/infoScore.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class infoScore : MonoBehaviour {
+
+	// test pour instancier les textes
+    
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
