293aad59037521531cdec43ec120f5059da04340
Kkkkkkkevin <kev.l.50@hotmail.fr>
2016-12-04 13:06:34 +0100

    scène tuto

---
+++ Assets/Scripts/TwoStartForPlay.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TwoStartForPlay : MonoBehaviour {
+
+    public GameObject startText_Joueur1;
+    public GameObject startText_Joueur2;
+
+    private bool startJ1 = false;
+    private bool startJ2 = false;
+
+    void Update () {
+        if (Input.GetButton("Start_J1"))
+        { 
+            //print("StartJ1 pressed !");
+            startJ1 = true;
+            startText_Joueur1.GetComponent<Text>().color = new Color(255, 0, 0);
+        }
+        if (Input.GetButton("Start_J2"))
+        {
+            //print("StartJ2 pressed !");
+            startJ2 = true;
+            startText_Joueur2.GetComponent<Text>().color = new Color(255, 0, 0);
+        }
+
+        if(startJ1 && startJ2)
+        {
+            Application.LoadLevel("Main");
+        }
+    }
+}
+
