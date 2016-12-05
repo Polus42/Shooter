66db2a1759aed34b27dac6cc27f251f890fb253d
Kkkkkkkevin <kev.l.50@hotmail.fr>
2016-12-05 14:05:21 +0100

    générique ajouté

---
+++ Assets/Scripts/changeScene_generq.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class changeScene_generq : MonoBehaviour {
+
+    void Start()
+    {
+        StartCoroutine(WaitAndPrint());
+    }
+
+    IEnumerator WaitAndPrint()
+    {
+        // suspend execution for 5 seconds
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("Menu");
+    }
+
+
+}
